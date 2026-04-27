87fa7ea510ee23800ce0d693c96e31ba96bc17a4
PIYUSH SONI 🕸 <piyushsoni7777777@gmail.com>
2026-04-27 20:54:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ function resolveCollision(a, b) {
 }
 
 // ─────────────────────────────────────────
-//  HELPER
+//  HELPER develope by piyushSni
 // ─────────────────────────────────────────
 function dist(x1, y1, x2, y2) { return Math.hypot(x1 - x2, y1 - y2); }
 
